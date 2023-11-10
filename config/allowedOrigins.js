aed868353368e6b7b5a1eb4330109568ecd10757
s-mcmartin <shannoncmcguire@outlook.com>
2023-11-10 14:32:58 -0500

    add: allowedOrigin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["https://sm-portfolio-frontendclient.onrender.com"];
+const allowedOrigins = [
+  "https://sm-portfolio-frontendclient.onrender.com",
+  "http://localhost:3000",
+];
 
 module.exports = allowedOrigins;
